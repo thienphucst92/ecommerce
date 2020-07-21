92ebd227c310d111f73ff23762d5355c35cb7679
Phuc Tran <phuctran@ITRs-MBP.localdomain>
2020-07-21 09:10:25 +0700

    modify start script

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 echo "Wait for services to start"
 docker-compose build
-docker-compose up -d
-docker-compose up -d 
-docker-compose up -d
+docker-compose up
 echo "Navigate to http://localhost:6776/graphql to play with the GraphQL Playground"
